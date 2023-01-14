d3893416c6efd053aca84feb4f71b9e825cb9eea
lxuegod <lxuegod@163.com>
2023-01-14 13:25:43 +0800

    完成文章接口

---
+++ app/admin/apis/article.go
@@ -0,0 +1,20 @@
+package apis
+
+import (
+	"github.com/gin-gonic/gin"
+	"go-admin/common/apis"
+)
+
+type Article struct {
+	apis.Api
+}
+
+// GetArticleList 获取文章列表
+func (e Article) GetArticleList(c *gin.Context) {
+	err := e.MakeContext(c).Errors
+	if err != nil {
+		e.Logger.Error(err)
+		return
+	}
+	e.OK("hello world ！", "success")
+}
